c26a04f9b8d9e6f07f6606c5d8f3e084199ba98e
Sean Collins <sean.collins@outlook.com>
2025-01-28 20:38:58 -0800

    Add additional parameters and update payload to support POST method

--- index.js
+++ index.js
@@ -8,16 +8,33 @@ export const options = {
 
 export default function () {
   // Define the endpoint URL
-  const url = 'https://test-api.k6.io/public/crocodiles/';
+  const BASE_URL = 'https://test-api.k6.io/';
+  const endpoint = `${BASE_URL}/public/crocodiles/`
 
-  // Send a GET request to the endpoint
-  const response = http.get(url);
+  // Example payload and headers
+  const payload = JSON.stringify({
+    test: 'data',
+    timestamp = new Date().toISOString(),
+  });
+
+  // Define parameters
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer [token]',
+    },
+  };
+
+  // Send a POST request to the endpoint
+  const response = http.post(endpoint, payload, params);
 
   // Check if the response is HTTP 200 (OK)
   check(response, {
     'is status 200': (r) => r.status === 200,
+    'response time < 200ms': (r) => r.timings.duration < 200,
+    'response includes success': (r) => r.json().success === true,
   });
 
   // Sleep for a short random duration between requests
-  sleep(Math.random() * 1 + 0.5); // 0.5 to 1.5s
+  sleep(Math.random() * 4 + 1); // 1 to 5s
 }
