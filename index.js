aed6c174301fcc7b26041a4d5cccf375ee1f67d7
Sean Collins <sean.collins@outlook.com>
2025-01-28 20:42:27 -0800

    Update options to support stages and thresholds

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 10, // Number of virtual users
-  duration: '30s', // Duration of the test
+  stages: [
+    { duration: '30s', target: 20 }, // Ramp up to 20 users over 30 seconds
+    { duration: '1m', target: 20 },  // Stay at 20 users for 1 minute
+    { duration: '30s', target: 0 },  // Ramp down to 0 users over 30 seconds
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
+    http_req_failed: ['rate<0.01'],   // Less than 1% of requests should fail
+  },
 };
 
 export default function () {
